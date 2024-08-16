0f0f0f61aeaa04d7f7869cc5163f3c37dd08ed86
3Harang <jungjaeseung9475@gmail.com>
2024-08-16 10:40:19 +0900

    tailwindcss config [브랜드컬러셋, safelist]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,16 +16,25 @@ const config: Config = {
       },
       backgroundColor: {
         "background-body": "#f2f4f7",
+        "background-default-subtle": "#fcfcfd",
         "components-main-nav-nav-button-bg-hover": "#1018280a",
         "components-main-nav-nav-button-bg-active": "#fcfcfd",
+        "divider-subtle": "#1018280a",
+        "state-accent-active": "#155aef14",
       },
       textColor: {
         "components-main-nav-nav-button-text": "#495464",
         "components-main-nav-nav-button-text-active": "#155aef",
+        "components-menu-item-text": "#495464",
+        "components-menu-item-text-hover": "#354052",
         "primary-600": "rgb(21 94 239)",
+        "text-accent": "#155aef",
       },
       boxShadow: {
         xs: "0px 1px 2px 0px rgba(16, 24, 40, .05)",
+      },
+      borderColor: {
+        "divider-burn": "#1018280a",
       },
     },
   },
@@ -37,6 +46,12 @@ const config: Config = {
     "text-sm",
     "text-[13px]",
     "text-[14px]",
+    "w-fit",
+    "grow",
+    "px-2.5",
+    "py-4",
+    "p-4",
+    "bg-emerald-100",
   ],
 };
 export default config;
